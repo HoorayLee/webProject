1d2a5709ae8489360a0880b460b8ed524b1ca640
郭士嘉 <897750289@qq.com>
2014-12-10 23:41:27 +0800

    v1.0

--- java/com/database/FileDAOImp.java
+++ java/com/database/FileDAOImp.java
@@ -12,7 +12,39 @@ import java.util.Iterator;
 
 public class FileDAOImp implements FileDAO, Serializable{
 
-	
+	@SuppressWarnings("finally")
+	@Override
+	public int returnNextUseFileID() throws Exception {
+		int nextFileID;
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		String sql = "SELECT MAX(FIleID) FROM file";
+		try
+		{
+			
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql);
+			ResultSet result = st.executeQuery(sql);
+			result.next();
+			nextFileID = result.getInt(1);
+			nextFileID++;
+			
+			st.close();
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp delete");
+			
+		}
+		finally
+		{
+			conn.close();
+			
+		}	
+		return nextFileID;
+	}
+	  
 	public void insert(Resource file) throws Exception {
 		String sql = "insert into File(FileName,AuthorID,UploadTime,Size,Institute,Grade,CourseName,Description) values (?,?,now(),?,?,?,?,?)";
 		DataBaseConnection conn = null;
@@ -75,8 +107,285 @@ public class FileDAOImp implements FileDAO, Serializable{
 		
 	}
 	  
-	public ArrayList<Resource> query_by_authorID(String user_id) throws Exception {
 	
+	public void update_DownloadNum(int file_id)
+			throws Exception {
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		String sql_query_download = "select DownloadNum from File WHERE FileID = ?";
+		String sql = "UPDATE File SET DownloadNum=? WHERE FileID=?";
+		int downloadNum ;
+		try
+		{
+			
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql_query_download);
+			st.setInt(1,file_id);
+			ResultSet result= st.executeQuery();
+			result.next();
+			downloadNum  = result.getInt(1);
+			st.close();
+			conn.close();
+			
+			downloadNum++;
+			conn  = new DataBaseConnection();
+			st = conn.getConnection().prepareStatement(sql);
+			st.setInt(1,downloadNum);
+			st.setInt(2,file_id);
+			
+			st.executeUpdate();
+			st.close();
+			
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp update_DownloadNum");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		
+		
+	}
+
+
+	public void update_Praise(int file_id) throws Exception {
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		String sql_query_Praise = "select Praise from File WHERE FileID = ?";
+		String sql = "UPDATE File SET Praise=? WHERE FileID=?";
+		int Praise ;
+		try
+		{
+			
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql_query_Praise);
+			st.setInt(1,file_id);
+			ResultSet result= st.executeQuery();
+			result.next();
+			Praise  = result.getInt(1);
+			st.close();
+			conn.close();
+			
+			Praise++;
+			conn  = new DataBaseConnection();
+			st = conn.getConnection().prepareStatement(sql);
+			st.setInt(1,Praise);
+			st.setInt(2,file_id);
+			
+			st.executeUpdate();
+			st.close();
+			
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp update_Praise");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		
+		
+	}
+
+	public void update_Criticize(int file_id) throws Exception {
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		String sql_query_Criticize = "select Criticize from File WHERE FileID = ?";
+		String sql = "UPDATE File SET Criticize=? WHERE FileID=?";
+		int Criticize ;
+		try
+		{
+			
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql_query_Criticize);
+			st.setInt(1,file_id);
+			ResultSet result= st.executeQuery();
+			result.next();
+			Criticize  = result.getInt(1);
+			st.close();
+			conn.close();
+			
+			Criticize++;
+			conn  = new DataBaseConnection();
+			st = conn.getConnection().prepareStatement(sql);
+			st.setInt(1,Criticize);
+			st.setInt(2,file_id);
+			
+			st.executeUpdate();
+			st.close();
+			
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp update_Criticize");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		
+		
+	}
+
+
+
+	
+	public ArrayList<Resource> query_by_requests(String grade, String className) throws Exception {
+		
+		String sql = "select * from File WHERE Grade =? and CourseName=?";
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		ArrayList<Resource> all = new ArrayList<Resource>();
+		try
+		{
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql);
+		    st.setString(1,grade);
+		    st.setString(2,className );
+			ResultSet result= st.executeQuery();
+			while(result.next())
+			{
+				Resource file = new Resource();
+				file.setsFileName(result.getString(2));
+				file.setsAuthorID(result.getString(3));
+				file.setdUploadTime(result.getTimestamp(4));
+				file.setiDownloadNum(result.getInt(5));
+				file.setlSize(result.getLong(6));
+				file.setiPraise(result.getInt(7));
+				file.setiCriticize(result.getInt(8));
+				file.setsInstitute(result.getString(9));
+				file.setiGrade(result.getInt(10));
+				file.setsCourseName(result.getString(11));
+				file.setsDescription(result.getString(12));
+				all.add(file);
+			}
+			result.close();
+			st.close();
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp search by grade and coursename ");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		return all;
+	}
+
+	@Override
+	public ArrayList<Resource> query_by_requests_bygrade(String grade)
+			throws Exception {
+		String sql = "select * from File WHERE Grade =? ";
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		ArrayList<Resource> all = new ArrayList<Resource>();
+		try
+		{
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql);
+		    st.setString(1,grade);
+		   
+			ResultSet result= st.executeQuery();
+			while(result.next())
+			{
+				Resource file = new Resource();
+				file.setsFileName(result.getString(2));
+				file.setsAuthorID(result.getString(3));
+				file.setdUploadTime(result.getTimestamp(4));
+				file.setiDownloadNum(result.getInt(5));
+				file.setlSize(result.getLong(6));
+				file.setiPraise(result.getInt(7));
+				file.setiCriticize(result.getInt(8));
+				file.setsInstitute(result.getString(9));
+				file.setiGrade(result.getInt(10));
+				file.setsCourseName(result.getString(11));
+				file.setsDescription(result.getString(12));
+				all.add(file);
+			}
+			result.close();
+			st.close();
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp search by request only grade");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		return all;
+	}
+
+
+	@Override
+	public ArrayList<Resource> query_by_requests_byname(String className)
+			throws Exception {
+		String sql = "select * from File WHERE CourseName=?";
+		DataBaseConnection conn = null;
+		PreparedStatement st = null;
+		ArrayList<Resource> all = new ArrayList<Resource>();
+		try
+		{
+			conn  = new DataBaseConnection();
+		    st = conn.getConnection().prepareStatement(sql);
+		   
+		    st.setString(1,className );
+			ResultSet result= st.executeQuery();
+			while(result.next())
+			{
+				Resource file = new Resource();
+				file.setsFileName(result.getString(2));
+				file.setsAuthorID(result.getString(3));
+				file.setdUploadTime(result.getTimestamp(4));
+				file.setiDownloadNum(result.getInt(5));
+				file.setlSize(result.getLong(6));
+				file.setiPraise(result.getInt(7));
+				file.setiCriticize(result.getInt(8));
+				file.setsInstitute(result.getString(9));
+				file.setiGrade(result.getInt(10));
+				file.setsCourseName(result.getString(11));
+				file.setsDescription(result.getString(12));
+				all.add(file);
+			}
+			result.close();
+			st.close();
+		}
+		catch(Exception e)
+		{
+			
+			throw new Exception("Error in FileDAOImp search by request only CourseName");
+			
+		}
+		finally
+		{
+			conn.close();
+		}
+		
+		return all;
+	}
+	
+	
+	public ArrayList<Resource> query_by_authorID(String user_id) throws Exception {
+		
 		String sql = "select * from File WHERE AuthorID=?";
 		DataBaseConnection conn = null;
 		PreparedStatement st = null;
@@ -119,54 +428,6 @@ public class FileDAOImp implements FileDAO, Serializable{
 		
 		return all;
 	}
-	
-	public ArrayList<Resource> query_by_requests(String grade, String className) throws Exception {
-		
-		String sql = "select * from File WHERE Grade =? and CourseName=?";
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		ArrayList<Resource> all = new ArrayList<Resource>();
-		try
-		{
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    st.setString(1,grade );
-		    st.setString(2,className );
-			ResultSet result= st.executeQuery();
-			while(result.next())
-			{
-				Resource file = new Resource();
-				file.setsFileName(result.getString(2));
-				file.setsAuthorID(result.getString(3));
-				file.setdUploadTime(result.getTimestamp(4));
-				file.setiDownloadNum(result.getInt(5));
-				file.setlSize(result.getLong(6));
-				file.setiPraise(result.getInt(7));
-				file.setiCriticize(result.getInt(8));
-				file.setsInstitute(result.getString(9));
-				file.setiGrade(result.getInt(10));
-				file.setsCourseName(result.getString(11));
-				file.setsDescription(result.getString(12));
-				all.add(file);
-			}
-			result.close();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp search by request ");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		return all;
-	}
-
-
 	
 
 	
@@ -270,250 +531,19 @@ public class FileDAOImp implements FileDAO, Serializable{
 		
 		return all;
 	}
-
-
 	
-	public void update_DownloadNum(int file_id, int DownloadNum)
-			throws Exception {
-		
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		DownloadNum++;
-		String sql = "UPDATE File SET DownloadNum=? WHERE FileID=?";
-		try
-		{
-			
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    
-			
-			st.setInt(1,DownloadNum);
-			st.setInt(2,file_id );
 
-			st.executeUpdate();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp update_DownloadNum");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		
-		
-	}
-
-
-	public void update_Praise(int file_id, int Praise) throws Exception {
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		Praise++;
-		String sql = "UPDATE File SET Praise=? WHERE FileID=?";
-		try
-		{
-			
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    
-			
-			st.setInt(1,Praise);
-			st.setInt(2,file_id );
-
-			st.executeUpdate();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp update_Praise");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		
-	}
-	public void update_Criticize(int file_id, int Criticize) throws Exception {
-		
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		Criticize++;
-		String sql = "UPDATE File SET Criticize=? WHERE FileID=?";
-		try
-		{
-			
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    
-			
-			st.setInt(1,Criticize);
-			st.setInt(2,file_id );
-
-			st.executeUpdate();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp update_Criticize");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		
-	}
-	/*
-	public void update(Resource file) throws Exception {
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		String sql = "UPDATE File SET DownloadNum=?,Praise=?,Criticize=? WHERE FileID=?";
-		try
-		{
-			
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    
-			
-			st.setInt(1,file.getiDownloadNum());
-			st.setInt(2, file.getiPraise());
-			st.setInt(3, file.getiCriticize());
-			st.setInt(4, file.getiFileID());
-			
-			
-			st.executeUpdate();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp update");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-	}
-	*/
-
-
-	@Override
-	public ArrayList<Resource> query_by_requests_bygrade(String grade)
-			throws Exception {
-		String sql = "select * from File WHERE Grade =? ";
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		ArrayList<Resource> all = new ArrayList<Resource>();
-		try
-		{
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		    st.setString(1,grade );
-		   
-			ResultSet result= st.executeQuery();
-			while(result.next())
-			{
-				Resource file = new Resource();
-				file.setsFileName(result.getString(2));
-				file.setsAuthorID(result.getString(3));
-				file.setdUploadTime(result.getTimestamp(4));
-				file.setiDownloadNum(result.getInt(5));
-				file.setlSize(result.getLong(6));
-				file.setiPraise(result.getInt(7));
-				file.setiCriticize(result.getInt(8));
-				file.setsInstitute(result.getString(9));
-				file.setiGrade(result.getInt(10));
-				file.setsCourseName(result.getString(11));
-				file.setsDescription(result.getString(12));
-				all.add(file);
-			}
-			result.close();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp search by request ");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		return all;
-	}
-
-
-	@Override
-	public ArrayList<Resource> query_by_requests_byname(String className)
-			throws Exception {
-		String sql = "select * from File WHERE CourseName=?";
-		DataBaseConnection conn = null;
-		PreparedStatement st = null;
-		ArrayList<Resource> all = new ArrayList<Resource>();
-		try
-		{
-			conn  = new DataBaseConnection();
-		    st = conn.getConnection().prepareStatement(sql);
-		   
-		    st.setString(1,className );
-			ResultSet result= st.executeQuery();
-			while(result.next())
-			{
-				Resource file = new Resource();
-				file.setsFileName(result.getString(2));
-				file.setsAuthorID(result.getString(3));
-				file.setdUploadTime(result.getTimestamp(4));
-				file.setiDownloadNum(result.getInt(5));
-				file.setlSize(result.getLong(6));
-				file.setiPraise(result.getInt(7));
-				file.setiCriticize(result.getInt(8));
-				file.setsInstitute(result.getString(9));
-				file.setiGrade(result.getInt(10));
-				file.setsCourseName(result.getString(11));
-				file.setsDescription(result.getString(12));
-				all.add(file);
-			}
-			result.close();
-			st.close();
-		}
-		catch(Exception e)
-		{
-			
-			throw new Exception("Error in FileDAOImp search by request ");
-			
-		}
-		finally
-		{
-			conn.close();
-		}
-		
-		return all;
-	}
-	
-	
 	
 
 	/*
 	public static void main(String[] args) throws Exception {
 	
 	  FileDAOImp test = new FileDAOImp();
-	  test.update_Criticize(1,0 );
-	  test.update_Praise(1, 0);
-	  */
-	  
+	System.out.println(test.returnNextUseFileID()); 
+	}
+*/
+
+	
 	  
 	 /*
 	  ArrayList <Resource> all = test.recommend_file_byDownloadNum();
